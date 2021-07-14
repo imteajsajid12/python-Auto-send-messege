bd21adef8ed3d250fb1cdfe0f3de42527f6dc701
imteajsajid12 <imteajsajid1@gmail.com>
2021-07-14 18:04:43 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+import pyautogui
+while True:
+    pyautogui.typewrite('hello')
+    time.sleep(3)
+    pyautogui.press('enter')hello
+    hello
+
+
+hello
+hello
+hello
+
+hello
+hello
